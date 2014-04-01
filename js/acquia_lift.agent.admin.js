485d65061469961e654e5d7c04bc4c3dfaa39cc5
Lisa Backer <lisa.backer@acquia.com>
2014-04-01 15:58:53 -0400

    Issue #2230197 by eshta: Follow-up to improve responsiveness of percentage control.

--- js/acquia_lift.agent.admin.js
+++ js/acquia_lift.agent.admin.js
@@ -10,22 +10,38 @@
         var $input = $('input.acquia-lift-percentage-control', this);
 
         var updateRest = function() {
-          var newPercent = $input.val();
-          var newRest = 100 - newPercent;
-          $rest.text(newRest + '%');
+          var newPercent = parseFloat($input.val());
+          if (isNaN(newPercent) || newPercent < 0 || newPercent > 100) {
+            $rest.text(Drupal.t('Invalid percentage'));
+            $rest.addClass('error');
+          } else {
+            // Limit the "rest" display to 2 decimal places.
+            var newRest = (100 - newPercent).toFixed(2);
+            // Calling parseFloat again here to trim off any trailing zeros.
+            $rest.text(parseFloat(newRest) + '%');
+            $rest.removeClass('error');
+          }
         }
-
-        // Limit input to numbers and decimal sign.
-        $('input.acquia-lift-percentage-control',this).keyup(function(e) {
-          var code = e.keyCode || e.which;
-          // Don't apply behavior to tab, enter, or arrow keys.
-          if (code == 9 || code == 13 || (code >= 37 && code <= 40)) { return; }
-
-          this.value = this.value.replace(/[^0-9\.]/g, '');
-        });
 
         // Update the "rest" percentage box when percentage changes.
         $('input.acquia-lift-percentage-control', this).change(updateRest);
+
+        // Add a timer to update the "rest" while the user is in the percentage
+        // input box.
+        var updateTimeout = NaN;
+        function clearUpdateTimeout() {
+          if (!isNaN(updateTimeout)) {
+            clearInterval(updateTimeout);
+            updateTimeout = NaN;
+          }
+        }
+
+        $('input.acquia-lift-percentage-control', this).focusin(function(e) {
+          clearUpdateTimeout();
+          updateTimeout = setInterval(updateRest, 1000);
+        });
+
+        $('input.acquia-lift-percentage-control', this).focusout(clearUpdateTimeout);
 
         // Set initial "rest" percentage value.
         updateRest();
@@ -45,13 +61,13 @@
         $container.prepend('<a class="acquia-lift-toggle-text" href="#"></a>');
 
         $('.acquia-lift-toggle-text', this).click(function(e) {
-          if ($container.hasClass('collapsed')) {
+          if ($container.hasClass('acquia-lift-collapsed')) {
             // Open and show additional details.
             $('.description', $container).slideDown();
-            $container.removeClass('collapsed');
+            $container.removeClass('acquia-lift-collapsed');
             $(this).text(Drupal.t('Hide info'));
           } else {
-            $container.addClass('collapsed');
+            $container.addClass('acquia-lift-collapsed');
             $('.description', $container).slideUp();
             $(this).text(Drupal.t('Info'));
           }
@@ -59,7 +75,7 @@
         });
 
         // Hide descriptions if collapsed by default.
-        if ($container.hasClass('collapsed')) {
+        if ($container.hasClass('acquia-lift-collapsed')) {
           $('.description', $container).hide();
           $('.acquia-lift-toggle-text', this).text(showText);
         } else {

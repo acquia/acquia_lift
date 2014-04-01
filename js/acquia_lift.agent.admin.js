e5d2eb66baca43e66ed614656b2d7be074649028
Lisa Backer <lisa.backer@acquia.com>
2014-04-01 11:39:25 -0400

    CH-338: Update percentage rest numbers after timeout and validate entry.

--- js/acquia_lift.agent.admin.js
+++ js/acquia_lift.agent.admin.js
@@ -10,22 +10,38 @@
         var $input = $('input.acquia-lift-percentage-control', this);
 
         var updateRest = function() {
-          var newPercent = $input.val();
-          var newRest = 100 - newPercent;
-          $rest.text(newRest + '%');
+          var newPercent = parseFloat($input.val());
+          if (isNaN(newPercent) || newPercent < 0 || newPercent > 100) {
+            $rest.text(Drupal.t('Invalid percentage'));
+            $rest.addClass('error');
+          } else {
+            // Limit the "rest" display to 2 decimal places.
+            var newRest = (100 - newPercent).toFixed(2);
+            // Calling parseFloat again here to trim off any trailing zeros.
+            $rest.text(parseFloat(newRest) + '%');
+            $rest.removeClass('error');
+          }
         }
-
-        // Limit input to numbers and decimal sign.
-        $('input.acquia-lift-percentage-control',this).keyup(function(e) {
-          var code = e.keyCode || e.which;
-          // Don't apply behavior to tab, enter, or arrow keys.
-          if (code == 9 || code == 13 || (code >= 37 && code <= 40)) { return; }
-
-          this.value = this.value.replace(/[^0-9\.]/g, '');
-        });
 
         // Update the "rest" percentage box when percentage changes.
         $('input.acquia-lift-percentage-control', this).change(updateRest);
+
+        // Add a timer to update the "rest" while the user is in the percentage
+        // input box.
+        var updateTimeout = NaN;
+        function clearUpdateTimeout() {
+          if (!isNaN(updateTimeout)) {
+            clearInterval(updateTimeout);
+            updateTimeout = NaN;
+          }
+        }
+
+        $('input.acquia-lift-percentage-control', this).focusin(function(e) {
+          clearUpdateTimeout();
+          updateTimeout = setInterval(updateRest, 1000);
+        });
+
+        $('input.acquia-lift-percentage-control', this).focusout(clearUpdateTimeout);
 
         // Set initial "rest" percentage value.
         updateRest();

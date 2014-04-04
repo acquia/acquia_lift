55a9359981c9f2ca85b0e2dd11cafb5f748de2fa
Katherine Bailey <katherine@katbailey.net>
2014-04-04 15:51:30 -0400

    Issue #2233759: Make it possible to reset all data for an agent from the campaign page

--- js/acquia_lift.admin.js
+++ js/acquia_lift.admin.js
@@ -14,4 +14,23 @@
     }
   };
 
+
+  /**
+   * Campaign edit form behaviors.
+   */
+  Drupal.behaviors.acquiaLiftCampaignEdit = {
+    attach: function (context, settings) {
+      // Add a handler to the "Reset data" button to warn the user before resetting the data.
+      $('#personalize-acquia-lift-reset input[type="submit"]').once('acquia-lift-reset').each(function() {
+        // Overwrite beforeSubmit of the ajax event.
+        Drupal.ajax[this.id].options.beforeSubmit = function(form_values, $element, options) {
+          if (confirm(Drupal.t('This action will delete all existing data for this campaign and cannot be undone. Are you sure you want to continue?'))) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      });
+    }
+  }
 })(jQuery);

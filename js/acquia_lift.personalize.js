b00af7bbe765a975f176c308f85814c55016aae7
Lisa Backer <lisa.backer@acquia.com>
2014-04-21 10:20:37 -0400

    CH-400: Standardize text for reporting link

--- js/acquia_lift.personalize.js
+++ js/acquia_lift.personalize.js
@@ -806,7 +806,7 @@ $.extend(Drupal.acquiaLiftUI, {
         this.$el
           .find('a[href]')
           .attr('href', activeCampaign.get('links').report)
-          .text(Drupal.t('Results for !agent', {'!agent': label}))
+          .text(Drupal.t('Reports'))
           .end()
           .show();
       }

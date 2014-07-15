539c4aa24abeb38a8f433a7be18edc68f80a5bdb
Lisa Backer <lisa.backer@acquia.com>
2014-07-15 07:44:07 -0400

    Clean up close handling and repeated calls.

--- js/acquia_lift.messagebox.js
+++ js/acquia_lift.messagebox.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, Drupal) {
 
   /**
    * JavaScript regarding the message box that is used for administrative
@@ -12,20 +12,42 @@
    *   The jQuery message box.
    */
   function createMessageBox() {
-    var $messageBox = $('#acquia-lift-message-box');
+    var $messageBox = getMessageBox();
     if ($messageBox.length == 0) {
-      $messageBox = $('<div id="acquia-lift-message-box"><div class="close">X</div><p class="message"></p></div>');
+      $messageBox = $('<div id="acquia-lift-message-box"><div class="close">' + Drupal.t('Close') + '</div><p class="message"></p></div>');
       $('.region-page-top').append($messageBox);
-      $messageBox.find('.close').on('click', function closeMessageBox() {
-        $messageBox.animate({ top:"+=15px", opacity:0 }, "slow", function() {
-          $(this).addClass('element-hidden')
-        });
+      $messageBox.find('.close').on('click', closeMessageBox);
+      // Don't close the message box if you click on it (other than close).
+      $messageBox.on('click', function(e) {
+        e.stopPropagation();
       });
     }
-    else {
-      $messageBox.removeClass('element-hidden');
-    }
+    return $messageBox;
+  }
 
+  /**
+   * Close the message box.
+   *
+   * @param e
+   *   The event that triggered the close.
+   */
+  function closeMessageBox(e) {
+    $messageBox = getMessageBox();
+    $messageBox.animate({ height:0, opacity:0 }, "slow", function() {
+      $(this).addClass('element-hidden');
+      // Take off the height/opacity styles - only used for animation.
+      $(this).removeAttr('style');
+    });
+    e.stopPropagation();
+  }
+
+  /**
+   * Helper function to retrieve the message box from the DOM if it exists.
+   *
+   * @returns jQuery match for message box selector
+   */
+  function getMessageBox() {
+    return $('#acquia-lift-message-box');
   }
 
   /**
@@ -36,13 +58,25 @@
   function showMessageBox(message) {
     var $messageBox = createMessageBox();
     $messageBox.find('.message').html(message);
+    // Measure the final height while the box is still hidden.
+    var fullHeight = $messageBox.height();
+    // Reset the properties to animate so that it starts hidden.
+    $messageBox.css('height', '0px');
+    $messageBox.css('opacity', '0');
+    $messageBox.removeClass('element-hidden');
+    // Animate the box height and opacity to draw attention.
+    $messageBox.animate({height: fullHeight + 'px', opacity: 1}, 'slow');
+
+    // Close the message box by clicking anywhere on the page.
+    $(document).one('click', function(e) {
+      closeMessageBox(e);
+    });
   }
 
   /**
    * A Drupal AJAX command to display a message box.
    */
   Drupal.ajax.prototype.commands.acquia_lift_message_box = function (ajax, response, status) {
-    Drupal.ajax.prototype.commands.acquia_lift_message_box = null;
     showMessageBox(response.data);
   };
-})(jQuery);
+})(jQuery, Drupal);

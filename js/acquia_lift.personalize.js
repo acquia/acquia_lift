96dbb498f102bcf2d0da4f5c2d4b84e8bdd0c89f
Lisa Backer <lisa.backer@acquia.com>
2014-08-06 13:35:54 -0400

    Allow empty campaigns to still have create variation menu capabilities.

--- js/acquia_lift.personalize.js
+++ js/acquia_lift.personalize.js
@@ -1114,9 +1114,15 @@
             // The first option is always control so the numbering displayed
             // actually matches the index number.
             var variationNumber = Math.max(nextIndex, 1);
+            if (nextIndex == 0) {
+              // Add a control variation display as well.
+              this.$el.find('ul.menu').append(Drupal.theme('acquiaLiftNewVariationMenuItem', -1));
+            }
             this.$el.find('ul.menu').append(Drupal.theme('acquiaLiftNewVariationMenuItem', variationNumber));
+            this.$el.find('ul.menu li.acquia-lift-empty').hide();
             // Indicate in the model that we are adding.
             this.model.set('activeVariation', -1);
+            this.render(this.model);
           } else {
             // If in edit mode, make sure that the edited variation index is
             // indicated.
@@ -1126,6 +1132,7 @@
           updateNavbar();
         } else {
           // If exiting, remove any temporary variation listings.
+          this.$el.find('ul.menu li.acquia-lift-empty').show();
           this.$el.find('.acquia-lift-page-variation-new').closest('li').remove();
         }
       }
@@ -2025,59 +2032,6 @@
           model: model
         }));
       }
-    },
-
-    /**
-     * Factory method to create the correct type of campaign model based
-     * on the type of data.
-     *
-     * @param object data
-     *   The data to create the model from.
-     */
-    createCampaignModel: function (data) {
-      if (data.type == 'acquia_lift_simple_ab') {
-        return new Drupal.acquiaLiftUI.MenuCampaignABModel(data);
-      } else {
-        return new Drupal.acquiaLiftUI.MenuCampaignModel(data);
-      }
-    }
-  };
-
-  /**
-   * Factory methods.
-   */
-  Drupal.acquiaLiftUI.factories = Drupal.acquiaLiftUI.factories || {};
-  Drupal.acquiaLiftUI.factories.MenuFactory = Drupal.acquiaLiftUI.factories.MenuFactory || {
-    /**
-     * Factory method to create the correct type of content variation set view
-     * based on the type of data displayed.
-     *
-     * @param Drupal.acquiaLiftUI.MenuOptionSetModel model
-     *   The model that will be the base for this view.
-     * @param Drupal.acquiaLiftUI.MenuCampaignModel model
-     *   The campaign model that owns the option set.
-     * @param element
-     *   The DOM element for the Backbone view.
-     */
-    createContentVariationView: function (model, campaignModel, element) {
-      if (campaignModel instanceof Drupal.acquiaLiftUI.MenuCampaignABModel) {
-        // There is only one page variation view per page per campaign, but
-        // this may be called multiple times due to multiple option sets.
-        Drupal.acquiaLiftUI.views.pageVariations = Drupal.acquiaLiftUI.views.pageVariations || {};
-        var campaignName = campaignModel.get('name');
-        if (!Drupal.acquiaLiftUI.views.pageVariations.hasOwnProperty(campaignName)) {
-          Drupal.acquiaLiftUI.views.pageVariations[campaignName] = new Drupal.acquiaLiftUI.MenuPageVariationsView({
-            model: campaignModel,
-            el: element
-          });
-        }
-      } else {
-        view = new Drupal.acquiaLiftUI.MenuOptionSetView({
-          model: model,
-          el: element
-        });
-      }
-      return view;
     },
 
     /**

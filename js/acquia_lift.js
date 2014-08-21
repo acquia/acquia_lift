3c0a297df2189a44bd36fee69090cbb2e65c63b4
Lisa Backer <lisa.backer@acquia.com>
2014-08-21 15:49:45 -0400

    Allow simple AB acquia lift agent to use the same JS agent as acquia_lift

--- js/acquia_lift.js
+++ js/acquia_lift.js
@@ -18,6 +18,7 @@ Drupal.personalize.agents.acquia_lift = {
     };
   }
 };
+Drupal.personalize.agents.acquia_lift_simple_ab = Drupal.personalize.agents.acquia_lift;
 
 /**
  * Adapter for the Acquia Lift API.

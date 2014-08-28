2de95358d31434083c36d520ece28b74fceeaf92
Al Steffen <al@zarabadoo.org>
2014-08-28 13:29:40 -0400

    Merge legend into the first cells of the table.

--- js/acquia_lift.report.ab.js
+++ js/acquia_lift.report.ab.js
@@ -514,7 +514,6 @@ Rickshaw.Graph.ClickDetail = Rickshaw.Class.create(Rickshaw.Graph.HoverDetail, {
 Rickshaw.namespace('Rickshaw.Graph.TableLegend');
 
 Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
-  // className: 'rickshaw_legend',
 
   initialize: function(args) {
     this.element = args.element;
@@ -527,14 +526,10 @@ Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
     // behavior of the series toggle
     this.graph.onUpdate( function() {} );
   },
-
   render: function() {
     var $ = jQuery,
         self = this,
         $label = $(this.element).find('thead > tr > th:first-child');
-
-    // Add a new column label for the legend.
-    $(this.element).find('thead > tr').prepend('<th>Legend</th>');
 
     this.lines = [];
 
@@ -550,38 +545,37 @@ Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
         self = this;
 
     $(this.element).find('tbody > tr').each(function (index, row) {
+      var $cell = $(row).find('td:first-child');
+
       if ($(row).find('td:first-child').text() == series.name) {
-        var line = document.createElement('td');
 
-        $(row).prepend(line);
-
-        line.className = 'legend line';
+        $cell.addClass('legend line');
         if (series.disabled) {
-          line.className += ' disabled';
+          $cell.addClass('disabled');
         }
         if (series.className) {
-          d3.select(line).classed(series.className, true);
+          d3.select($cell[0]).classed(series.className, true);
         }
+
+        var label = document.createElement('span');
+        label.className = 'label';
+        label.innerHTML = (series.shortName || series.name) + ': ';
+
+        $cell.prepend(label);
+
+        $cell[0].series = series;
 
         var swatch = document.createElement('div');
         swatch.className = 'swatch';
         swatch.style.backgroundColor = series.color;
 
-        line.appendChild(swatch);
-
-        var label = document.createElement('span');
-        label.className = 'label';
-        label.innerHTML = series.shortName || series.name;
-
-        line.appendChild(label);
-
-        line.series = series;
+        $cell.prepend(swatch);
 
         if (series.noLegend) {
-          line.style.display = 'none';
+          $cell.css('display', 'none');
         }
 
-        var _line = { element: line, series: series };
+        var _line = { element: $cell[0], series: series };
         if (self.shelving) {
           self.shelving.addAnchor(_line);
           self.shelving.updateBehaviour();
@@ -590,7 +584,7 @@ Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
           self.highlighter.addHighlightEvents(_line);
         }
         self.lines.push(_line);
-        return line;
+        return $cell[0];
       }
     });
   }

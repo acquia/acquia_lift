e25942be002879867dc253407f653c9302b7bd60
Lisa Backer <lisa.backer@acquia.com>
2014-09-05 15:55:26 -0400

    Add outerHTML cleaner function.

--- js/acquia_lift.page_variations.js
+++ js/acquia_lift.page_variations.js
@@ -551,8 +551,42 @@
    */
   Drupal.acquiaLiftPageVariations.personalizeElements = Drupal.acquiaLiftPageVariations.personalizeElements || {};
   Drupal.acquiaLiftPageVariations.personalizeElements.editHtml = {
+    getOuterHtml: function($element) {
+      if ($element.length > 1) {
+        $element = $element.first();
+      }
+      // jQuery doesn't have an outerHTML so we need to clone the child and
+      // give it a parent so that we can call that parent's html function.
+      // This ensures we get only the html of the $selector and not siblings.
+      var $element = $element.clone().wrap('<div>').parent();
+      // Remove any extraneous acquia lift / visitor actions stuff.
+      var removeClasses = new RegExp(Drupal.settings.visitor_actions.ignoreClasses, 'g');
+      var removeId = new RegExp(Drupal.settings.visitor_actions.ignoreIds);
+      var removeTags = 'script';
+
+      // Remove any invalid ids.
+      $element.find('[id]').filter(function() {
+        return removeId.test(this.id);
+      }).removeAttr('id');
+
+      // Remove any classes that are marked for ignore.
+      $element.find('[class]').each(function() {
+        var stripClasses = this.className.match(removeClasses) || [];
+        $(this).removeClass(stripClasses.join(' '));
+        if (this.className.length == 0) {
+          $(this).removeAttr('class');
+        }
+      });
+      // Remove any styling added directly from jQuery.
+      $element.find('[style]').removeAttr('style');
+      // Remove any inappropriate tags
+      $element.find(removeTags).remove();
+
+      // Now return the cleaned up html.
+      return $element.html();
+    },
     editInContext : function(selector, $contentInput) {
-      var editString = Drupal.personalizeElements.editHtml.getOuterHtml($(selector));
+      var editString = this.getOuterHtml($(selector));
       $contentInput.val(editString);
     }
   };

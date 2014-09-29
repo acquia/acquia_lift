b75557a61c83204be44b7120c8ee0b50b5591425
Al Steffen <al@zarabadoo.org>
2014-09-29 12:04:05 -0400

    CH-908 by Zarabadoo: Change the metric value when the metric select is changes and make it persisist when a new goal is loaded.

--- js/acquia_lift.reports.js
+++ js/acquia_lift.reports.js
@@ -1000,41 +1000,6 @@ Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
 (function ($, Drupal) {
   "use strict";
 
-  /**
-   * Load a new graph report and format it as a graph.
-   *
-   * @params
-   * - $container: A jQuery object that the report will be appended to.
-   * - args: An object of filters to be applied to the results.
-   *    - campaign: The machine name for the campaign data to load.
-   *    - decision: The machine name for the desision to load.
-   *    - goal: The machine name of the goal to load.
-   *    - start: The unix timestamp for the start date of the report.
-   *    - end: The unix timestamp for the end date of the report.
-   */
-  var acquiaLiftLoadReport = function ($container, args) {
-    // Construct the GET path.
-    var path = Drupal.settings.basePath + 'acquia_lift/reports/conversion',
-        position = 0;
-    for (var arg in args) {
-      if (args.hasOwnProperty(arg)) {
-        if (args[arg].length >= 0) {
-          var prefix = position === 0 ? '?' : '&';
-          path += prefix + arg + '=' + args[arg];
-        }
-      }
-      position++;
-    }
-
-    // Get the new report and replace the existing report(s) with it.
-    $.get(path, function (data) {
-      var $data = $(data);
-      $container.find('.lift-statistic-category').remove();
-      $container.prepend($data);
-      $data.find('table[data-lift-statistics]').liftGraph();
-    });
-  };
-
   Drupal.behaviors.acquiaLiftReports = {
     attach: function (context, settings) {
       // Generate graphs and switches for the A/B statistics.
@@ -1042,49 +1007,68 @@ Rickshaw.Graph.TableLegend = Rickshaw.Class.create(Rickshaw.Graph.Legend, {
         var $statistics = $(this),
             $data = $statistics.find('table[data-lift-statistics]'),
             campaign = $data.attr('data-acquia-lift-campaign'),
-            attr = typeof $data.attr('data-liftgraph-excluded') !== 'undefined' ? $data.attr('data-liftgraph-excluded').split(',') : [],
-            variationColumn = $data.attr('data-liftgraph-columnname') - 1,
-            xColumn = $data.attr('data-liftgraph-columnx') - 1,
-            name = $data.attr('data-liftgraph-columnname') - 1,
             $goalSelect = $('.acquia-lift-report-section-options .form-item-goal select'),
             $metricSelect = $('.acquia-lift-report-section-options .form-item-metric select'),
             metric = $metricSelect.val(),
-            $metricOptions = $metricSelect.children('option');
+            $metricOptions = $metricSelect.children('option'),
+            // Find the tqble column that matches the metric value.
+            metricColumn = function() {
+              var $heads = $data.find('thead > tr > th'),
+                  match = 0,
+                  i = 1;
+              while (i < $heads.length) {
+                if ($($heads[i]).attr('data-conversion-metric') === metric) {
+                  match = i + 1;
+                  break;
+                }
+                i++;
+              }
+
+              return parseFloat(match);
+            };
 
         // Format the initial data.
         $data.liftGraph();
 
-        // The data-liftgraph-excluded attribute is a comma delimited list of
-        // column numbers starting at 1. It needs a bit of extra processing
-        // to make the individual strings from the split integers.
-        for (var i = 0; i < attr.length; i++) {
-          attr[i] = parseFloat(attr[i]);
-        }
-
         // Load a new report if the goal is changed.
         $goalSelect.change(function() {
-          acquiaLiftLoadReport($statistics, {campaign: campaign, goal: $(this).val()});
+          // Construct the GET path.
+          var path = Drupal.settings.basePath + 'acquia_lift/reports/conversion',
+              position = 0,
+              args = {
+                campaign: campaign,
+                goal: $(this).val()
+              };
+          for (var arg in args) {
+            if (args.hasOwnProperty(arg)) {
+              if (args[arg].length >= 0) {
+                var prefix = position === 0 ? '?' : '&';
+                path += prefix + arg + '=' + args[arg];
+              }
+            }
+            position++;
+          }
+
+          // Get the new report and replace the existing report(s) with it.
+          $.get(path, function (html) {
+            var $html = $(html);
+            $statistics.find('.lift-statistic-category').remove();
+            $statistics.prepend($html);
+            // Simultaneously replace $data with the new table.
+            $data = $html.find('table[data-lift-statistics]');
+            // Make sure the proper metric column is set and render the graph.
+            $data.attr('data-liftgraph-columny', metricColumn()).liftGraph();
+          });
         })
 
         // Attach a data column to a metric option.
         // Change the data fed to the y-axis and update the graph.
         $metricSelect.change(function () {
-          var value = $(this).val(),
-              $heads = $data.find('thead > tr > th'),
-              match = 0,
-              i = 1;
+          // Set the new metric value.
+          metric = $(this).val();
 
-          while (i < $heads.length) {
-            if ($heads.attr('data-conversion-metric') === value) {
-              match = i + 1;
-              break;
-            }
-            i++;
-          }
-
-          console.log(match);
-
-          $data.attr('data-liftgraph-columny', parseFloat(match))
+          // Change the data-liftgraph-columny attribute and rebuild the graph.
+          $data.attr('data-liftgraph-columny', metricColumn())
             .liftGraph('update');
         })
       });

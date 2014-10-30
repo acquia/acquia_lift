05da7aa365e75406191736ccb03c05741c82ef20
Lisa Backer <lisa.backer@acquia.com>
2014-10-30 15:08:04 -0400

    Issue #2366485 by eshta: Include visitor_actions_ui jquery ducktape.events.js file rather than duplicating.

--- js/jquery/ducktape.events.js
+++
@@ -1,7 +0,0 @@
-// Backbone has a dependency on jQuery 1.7+ because it refers to the
-// jQuery.fn.on and jQuery.fn.off methods. We polyfill them here, but prefer
-// the native implementation if it exists.
-if (!jQuery.fn.on && !jQuery.fn.off) {
-  jQuery.fn.on = jQuery.fn.bind;
-  jQuery.fn.off = jQuery.fn.unbind;
-}

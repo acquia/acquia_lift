ba1642a3289e83c65c560717c64a14cf4069696b
Katherine Bailey <katherine@katbailey.net>
2014-11-12 09:25:54 -0700

    Issue #2374013: Make it possible to capture visitor's identity from a querystring param and send to Lift Web

--- acquia_lift_profiles/qunit/tests.js
+++ acquia_lift_profiles/qunit/tests.js
@@ -6,8 +6,16 @@ QUnit.module("Acquia Lift Profiles", {
     Drupal.settings.personalize = Drupal.settings.personalize || {};
     Drupal.settings = Drupal.settings || {};
     Drupal.settings.acquia_lift_profiles = Drupal.settings.acquia_lift_profiles || {};
+    Drupal.settings.acquia_lift_profiles.udfMappings = {};
+    Drupal.settings.acquia_lift_profiles.udfMappingContextSeparator = '__';
+
     Drupal.personalize = Drupal.personalize || {};
     Drupal.personalize.visitor_context = Drupal.personalize.visitor_context || {};
+    // We need to mock the getVisitorContexts() method as this is called by the
+    // init method.
+    Drupal.personalize.getVisitorContexts = function(plugins, callback) {
+      callback.call(null, {});
+    };
     function assignDummyValues(contexts) {
       var values = {
         'some-context': 'some-value',
@@ -164,140 +172,175 @@ QUnit.test("get context values with cache", function( assert ) {
 });
 
 QUnit.asyncTest( "personalize decision event", function( assert ) {
-  expect(10);
+  expect(5);
   Drupal.acquia_lift_profiles.resetAll();
-  var personalizeDecisionPushCount = 0;
   _tcaq = {
-    'push':function(stf) {
-      console.log(stf);
-      if ( personalizeDecisionPushCount == 0 ) {
-        assert.equal( stf[0], 'captureView',  'capture view received');
-        assert.equal( stf[1], 'Content View',  'capture view is of type content view');
-        assert.equal( stf[2].person_udf1, "some-value", 'value correctly assigned from context' );
-        assert.equal( stf[2].person_udf2, "some-other-value", 'value correctly assigned from promise based context' );
-        assert.equal( stf[2].person_udf3, "some-other-value", 'same  value correctly assigned to a second UDF' );
-
-      }
-      else {
-        assert.equal( stf[0], 'capture',  'capture view received');
-        assert.equal( stf[1], 'Campaign Action',  'capture view is of type campaign action');
-        assert.equal( stf[2].campaignid, "my-agent", 'value correctly assigned from event' );
-        assert.equal( stf[2].campaignname, "Test Agent", 'value correctly assigned from event' );
-        assert.equal( stf[2].actionName, "test_decision", 'value correctly assigned from event' );
+    'push':function(args) {
+      if ( args[1] == 'Campaign Action' ) {
+        assert.equal( args[0], 'capture',  'capture view received');
+        assert.equal( args[1], 'Campaign Action',  'capture view is of type campaign action');
+        assert.equal( args[2].campaignid, "my-agent", 'value correctly assigned from event' );
+        assert.equal( args[2].campaignname, "Test Agent", 'value correctly assigned from event' );
+        assert.equal( args[2].actionName, "test_decision", 'value correctly assigned from event' );
         QUnit.start();
       }
-      personalizeDecisionPushCount++;
     }
   };
-  var settings = {
-    acquia_lift_profiles: {
-      udfMappings: {
-        person: {
-          person_udf1: "my_first_plugin__some-context",
-          person_udf2: "my_promise_plugin__some-other-context",
-          // Test that another UDF can be mapped to the same context.
-          person_udf3: "my_promise_plugin__some-other-context"
-        }
-      },
-      udfMappingContextSeparator: '__'
-    },
-    personalize : {
-      agent_map : {
-        'my-agent': {
-          'active': 1,
-          'cache_decisions': false,
-          'enabled_contexts': [],
-          'type': 'test_agent',
-          'label' : 'Test Agent'
-        }
-      }
+  var settings = jQuery.extend({}, Drupal.settings);
+  settings.personalize.agent_map = {
+    'my-agent': {
+      'active': 1,
+      'cache_decisions': false,
+      'enabled_contexts': [],
+      'type': 'test_agent',
+      'label' : 'Test Agent'
     }
   };
 
-  // We need to mock the getVisitorContexts() method as this is called by the
-  // init method, which we're testing here. We just need to make it call the
-  // callback that will be passed into it with the values for the contexts
-  // specified.
-  Drupal.personalize.getVisitorContexts = function(plugins, callback) {
-    var values = {
-      'my_first_plugin': {
-        'some-context': 'some-value'
-      },
-      'my_promise_plugin': {
-        'some-other-context': 'some-other-value'
-      }
-    };
-    callback.call(null, values);
-  };
   Drupal.acquia_lift_profiles.init(settings);
   $(document).trigger("personalizeDecision", [{}, "test_decision", "test_osid", "my-agent" ]);
 });
 
 QUnit.asyncTest( "sent goal to agent event", function( assert ) {
-  expect(9);
+  expect(4);
   Drupal.acquia_lift_profiles.resetAll();
-  var personalizeDecisionPushCount = 0;
   _tcaq = {
-    'push':function(stf) {
-      console.log(stf);
-      if ( personalizeDecisionPushCount == 0 ) {
-        assert.equal( stf[0], 'captureView',  'capture view received');
-        assert.equal( stf[1], 'Content View',  'capture view is of type content view');
-        assert.equal( stf[2].person_udf1, "some-value", 'value correctly assigned from context' );
-        assert.equal( stf[2].person_udf2, "some-other-value", 'value correctly assigned from promise based context' );
-        assert.equal( stf[2].person_udf3, "some-other-value", 'same  value correctly assigned to a second UDF' );
-
-      }
-      else {
-        assert.equal( stf[0], 'capture',  'capture received');
-        assert.equal( stf[1], 'goal-event',  'capture view is of type goal-event');
-        assert.equal( stf[2].campaignid, "my-agent", 'value correctly assigned from event' );
-        assert.equal( stf[2].campaignname, "Test Agent", 'value correctly assigned from event' );
+    'push':function(args) {
+      if ( args[1] == 'goal-event' ) {
+        assert.equal( args[0], 'capture',  'capture received');
+        assert.equal( args[1], 'goal-event',  'capture view is of type goal-event');
+        assert.equal( args[2].campaignid, "my-agent", 'value correctly assigned from event' );
+        assert.equal( args[2].campaignname, "Test Agent", 'value correctly assigned from event' );
         QUnit.start();
       }
-      personalizeDecisionPushCount++;
     }
   };
-  var settings = {
-    acquia_lift_profiles: {
-      udfMappings: {
-        person: {
-          person_udf1: "my_first_plugin__some-context",
-          person_udf2: "my_promise_plugin__some-other-context",
-          // Test that another UDF can be mapped to the same context.
-          person_udf3: "my_promise_plugin__some-other-context"
-        }
-      },
-      udfMappingContextSeparator: '__'
+  var settings = jQuery.extend({}, Drupal.settings);
+  settings.personalize.agent_map = {
+    'my-agent': {
+      'active': 1,
+      'cache_decisions': false,
+      'enabled_contexts': [],
+      'type': 'test_agent',
+      'label' : 'Test Agent'
+    }
+  };
+
+  Drupal.acquia_lift_profiles.init(settings);
+  $(document).trigger("sentGoalToAgent", ["my-agent", "goal-event", "goal-value"]);
+});
+
+QUnit.test( "Server-side events no email", function( assert ) {
+  expect(4);
+  Drupal.acquia_lift_profiles.resetAll();
+  // Add a couple of server-side actions to the settings. Don't include the 'mail'
+  // property in the user_login action's context.
+  var settings = jQuery.extend({}, Drupal.settings);
+  settings.acquia_lift_profiles.serverSideActions = {
+    'someAction' : {
+      'someContextKey': 'someContextValue'
     },
-    personalize : {
-      agent_map : {
-        'my-agent': {
-          'active': 1,
-          'cache_decisions': false,
-          'enabled_contexts': [],
-          'type': 'test_agent',
-          'label' : 'Test Agent'
-        }
+    'user_login': [
+      { 'username': 'someUser' }
+    ]
+  };
+
+  // Our _tcaq.push should be called with a 'capture' event for each server-side
+  // action in our settings when we call processServerSideActions. THere should not
+  // be a captureIdentity push because there's no email address in our user_login
+  // context.
+  _tcaq = {
+    'push':function(args) {
+      if ( args[1] == 'someAction' ) {
+        assert.equal( args[0], 'capture',  'capture received');
+        assert.equal( args[1], 'someAction',  'custom event captured');
+      }
+      else if (args[1] == 'user_login' ) {
+        assert.equal( args[0], 'capture',  'capture received');
+        assert.equal( args[1], 'user_login',  'custom event captured');
+      }
+      else if (args[0] == 'captureIdentity') {
+        // Add an assertion here which simply serves to make the test fail if we
+        // ever get here because the number of assertions will not match what's
+        // expected.
+        assert.ok('This should never run');
       }
     }
   };
 
-  // We need to mock the getVisitorContexts() method as this is called by the
-  // init method, which we're testing here. We just need to make it call the
-  // callback that will be passed into it with the values for the contexts
-  // specified.
-  Drupal.personalize.getVisitorContexts = function(plugins, callback) {
-    var values = {
-      'my_first_plugin': {
-        'some-context': 'some-value'
-      },
-      'my_promise_plugin': {
-        'some-other-context': 'some-other-value'
-      }
-    };
-    callback.call(null, values);
+  Drupal.acquia_lift_profiles.processServerSideActions(settings);
+});
+
+
+QUnit.test( "Server-side events with email", function( assert ) {
+  expect(7);
+  Drupal.acquia_lift_profiles.resetAll();
+
+  // Add a couple of server-side actions to the settings. This time we do include
+  // the 'mail' property in the user_login action's context.
+  var settings = jQuery.extend({}, Drupal.settings);
+  settings.acquia_lift_profiles.captureIdentity = 1;
+  settings.acquia_lift_profiles.serverSideActions = {
+    'someAction' : {
+      'someContextKey': 'someContextValue'
+    },
+    'user_login': [
+      { 'mail': 'test@example.com' }
+    ]
   };
+
+  // Our _tcaq.push should be called with a 'capture' event for each server-side
+  // action in our settings when we call processServerSideActions. It should also
+  // be called with a captureIdentity event because of the user_login with email.
+  _tcaq = {
+    'push':function(args) {
+      if ( args[1] == 'someAction' ) {
+        assert.equal( args[0], 'capture',  'capture received');
+        assert.equal( args[1], 'someAction',  'custom event captured');
+      }
+      else if (args[1] == 'user_login' ) {
+        assert.equal( args[0], 'capture',  'capture received');
+        assert.equal( args[1], 'user_login',  'custom event captured');
+      }
+      else if (args[0] == 'captureIdentity') {
+        assert.equal( args[0], 'captureIdentity',  'capture received');
+        assert.equal( args[1], 'test@example.com',  'identity captured');
+        assert.equal( args[2], 'email',  'identity type captured');
+      }
+    }
+  };
+  Drupal.acquia_lift_profiles.processServerSideActions(settings);
+});
+
+QUnit.test( "Capture identity test", function( assert ) {
+  expect(3);
+  Drupal.acquia_lift_profiles.resetAll();
+
+  var settings = jQuery.extend({}, Drupal.settings);
+  // Add an identity and identityType to the settings.
+  settings.acquia_lift_profiles.identity = 'someTestUser';
+  settings.acquia_lift_profiles.identityType = 'socialTastic';
+  // Our _tcaq.push should be called wiht a captureIdentity event
+  // when we call the init function with our settings.
+  _tcaq = {
+    'push':function(args) {
+      if ( args[0] == 'captureIdentity' ) {
+        assert.equal( args[0], 'captureIdentity',  'capture received');
+        assert.equal( args[1], 'someTestUser',  'identity captured');
+        assert.equal( args[2], "socialTastic", 'identity type received' );
+      }
+    }
+  };
+
   Drupal.acquia_lift_profiles.init(settings);
-  $(document).trigger("sentGoalToAgent", ["my-agent", "goal-event", "goal-value"]);
+
+  // There should be no subsequent captureIdentity call when we process a
+  // user login action.
+  settings.acquia_lift_profiles.captureIdentity = 1;
+  settings.acquia_lift_profiles.serverSideActions = {
+    'user_login': [
+      { 'mail': 'test@example.com' }
+    ]
+  };
+  Drupal.acquia_lift_profiles.processServerSideActions(settings);
 });

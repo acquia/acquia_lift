4fee4a01acaa5d55c9954b6f3e734a9d55edeee3
Lisa Backer <lisa.backer@acquia.com>
2015-01-09 15:20:41 -0500

    Issue #2404655 by eshta: Fix empty option set view to always listen for changes to the active campaign.

--- src/js/menu/acquia_lift.personalize.views.js
+++ src/js/menu/acquia_lift.personalize.views.js
@@ -456,6 +456,9 @@
       this.listenTo(this.collection, 'change', this.onActiveCampaignChange);
 
       this.build();
+
+      // Set the initial campaign listeners if available.
+      this.onActiveCampaignChange();
       this.render();
     },
 

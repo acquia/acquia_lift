c26ca7cb51917a0b0e38118386bc9bb49ed2ce84
Lisa Backer <lisa.backer@acquia.com>
2015-02-24 06:05:13 -0500

    Revert "Revert "Issue #2433903 by eshta: Fix 'no variations' text remains for AB campaigns until active campaign change.""
    This reverts commit a4c3118fbd9bd0e25551f30da3871d4d0f939203.

--- js/acquia_lift.personalize.js
+++ js/acquia_lift.personalize.js
@@ -1961,6 +1961,7 @@
       this.model = this.collection.findWhere({'isActive': true});
       if (this.model) {
         this.listenTo(this.model, 'change:optionSets', this.render);
+        this.listenTo(this.model, 'change:variations', this.render);
       }
       this.render();
     },

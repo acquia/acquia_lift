76f1227205b9f66423286de61539561a4e19f876
Lisa Backer <lisa.backer@acquia.com>
2015-03-16 13:48:55 -0400

    Allow audiences to be sortable based on weights.

---
+++ js/acquia_lift_target.admin.js
@@ -0,0 +1,36 @@
+/**
+ * @file acquia_lift_target.admin.js
+ *
+ * Functionality for the administration of Acquia Lift Target campaigns.
+ */
+
+(function ($, Drupal) {
+
+  Drupal.behaviors.acquiaLiftTargetAdmin = {
+    attach: function (context, settings) {
+      // Attach the drag 'n' drop behavior to audiences in order to determine
+      // audience weight.
+      $('#acquia-lift-targeting-audiences .el-card.is-sortable').once(function () {
+        $(this).parent().addClass('acquia-lift-sortable');
+        // Hide the weight form item as it will be dynamically determined.
+        $('.acquia-lift-sortable-weight', this).parent().hide();
+      });
+      // Make the audiences sortable
+      $('#acquia-lift-targeting-audiences').sortable({
+        items: "> .acquia-lift-sortable",
+        placeholder: 'acquia-lift-sortable-placeholder',
+        forcePlaceholderSize: true,
+        update: function (event, ui) {
+          // Update the weight values for each sortable element after
+          // the positions have been updated.
+          var weight = 10;
+          $('#acquia-lift-targeting-audiences .el-card.is-sortable').each(function() {
+            $('.acquia-lift-sortable-weight', this).val(weight);
+            weight += 10;
+          });
+        }
+      });
+    }
+  }
+
+})(Drupal.jQuery, Drupal);

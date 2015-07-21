67ef47de496f2e9fc926da26c58e06c0fb7a27d6
rachel mak <rachel.mak@acquia.com>
2015-07-21 10:06:31 -0400

    Lift Web Debugger - fixed merge conflicts, moved preview buttons, UI fixes

--- js/acquia_lift.debugger.js
+++ js/acquia_lift.debugger.js
@@ -1,5 +1,5 @@
 //var $ = JQuery;
-var app = angular.module("debuggerModule", ['isteven-multi-select', 'autocomplete']);
+var app = angular.module("debuggerModule", ['isteven-multi-select', 'autocomplete','angularResizable']);
 app.constant('liftwebURl', "");
 app.value('debugPrefix', 'acquiaLift::debug');
 app.constant('previewStatus', "Site Preview")
@@ -88,11 +88,11 @@ app.factory('debuggerFactory', function($http, liftwebURl){
 
             }
 
-            Lift.debugger.prototype.clickDestroy = function (event) {
-                Drupal.acquiaLiftProfilesDebug.clearStorage()
-                this.destroy();
+      Lift.debugger.prototype.clickDestroy = function (event) {
+        Drupal.acquiaLiftProfilesDebug.clearStorage();
+        this.destroy();
+      }
 
-            }
 
             Lift.debugger.prototype.setOpen = function () {
 
@@ -339,17 +339,17 @@ app.controller("DebuggerController", function($scope, $timeout, debuggerFactory,
         $scope.allSegments.push(item);
     }
 
-    $scope.startPreview = function(){
-        $sessionStorage.setObject(debugPrefix + "::originalSegments",$scope.profile.curSegments);
-        $sessionStorage.setObject(debugPrefix + "::overrideSegments",$scope.profile.overrideSegments);
-        Drupal.acquiaLiftProfilesDebug.setOverrideSegments($scope.profile.overrideSegments);    }
+  $scope.startPreview = function(){
+    $sessionStorage.setObject(debugPrefix + "::originalSegments",$scope.profile.curSegments);
+    $sessionStorage.setObject(debugPrefix + "::overrideSegments",$scope.profile.overrideSegments);
+    Drupal.acquiaLiftProfilesDebug.setOverrideSegments($scope.profile.overrideSegments);    }
 
-    $scope.stopPreview = function(){
-        $scope.profile.overrideSegments = $sessionStorage.getObject(debugPrefix + "::overrideSegments");
-        $scope.profile.curSegments = $sessionStorage.getObject(debugPrefix + "::originalSegments");
-        if ($scope.profile.overrideSegments && $scope.profile.overrideSegments.length > 0){
-            Drupal.acquiaLiftProfilesDebug.setOverrideSegments(null);
-            $sessionStorage.removeItem(debugPrefix + "::overrideSegments");
+  $scope.stopPreview = function(){
+    $scope.profile.overrideSegments = $sessionStorage.getObject(debugPrefix + "::overrideSegments");
+    $scope.profile.curSegments = $sessionStorage.getObject(debugPrefix + "::originalSegments");
+    if ($scope.profile.overrideSegments && $scope.profile.overrideSegments.length > 0){
+      Drupal.acquiaLiftProfilesDebug.setOverrideSegments(null);
+      $sessionStorage.removeItem(debugPrefix + "::overrideSegments");
         }
     }
 
@@ -391,7 +391,6 @@ app.controller("DebuggerController", function($scope, $timeout, debuggerFactory,
         }
     }
     $scope.buttonClick('log');
-
 });
 angular.module('debuggerModule')
     .filter('to_icon', ['$sce', function($sce){
@@ -1688,5 +1687,143 @@ app.directive('suggestion', function(){
         }
     };
 });
+angular.module('angularResizable', [])
+    .directive('resizable', function() {
+        var toCall;
+        function throttle(fun) {
+            if (toCall === undefined) {
+                toCall = fun;
+                setTimeout(function() {
+                    toCall();
+                    toCall = undefined;
+                }, 100);
+            } else {
+                toCall = fun;
+            }
+        }
+        return {
+            restrict: 'AE',
+            scope: {
+                rDirections: "=",
+                rCenteredX: "=",
+                rCenteredY: "=",
+                rWidth: "=",
+                rHeight: "=",
+                rFlex: "=",
+                rGrabber: "@",
+                rDisabled: "@"
+            },
+            link: function(scope, element, attr) {
 
+                // register watchers on width and height attributes if they are set
+                scope.$watch('rWidth', function(value){
+                    element[0].style.width = scope.rWidth + 'px';
+                });
+                scope.$watch('rHeight', function(value){
+                    element[0].style.height = scope.rHeight + 'px';
+                });
+
+                element.addClass('resizable');
+
+                var style = window.getComputedStyle(element[0], null),
+                    w,
+                    h,
+                    dir = scope.rDirections,
+                    vx = scope.rCenteredX ? 2 : 1, // if centered double velocity
+                    vy = scope.rCenteredY ? 2 : 1, // if centered double velocity
+                    inner = scope.rGrabber ? scope.rGrabber : '<span></span>',
+                    start,
+                    dragDir,
+                    axis,
+                    info = {};
+
+                var updateInfo = function(e) {
+                    info.width = false; info.height = false;
+                    if(axis == 'x')
+                        info.width = scope.rFlex ? parseInt(element[0].style.flexBasis) : parseInt(element[0].style.width);
+                    else
+                        info.height = scope.rFlex ? parseInt(element[0].style.flexBasis) : parseInt(element[0].style.height);
+                    info.id = element[0].id;
+                    info.evt = e;
+                }
+
+                var dragging = function(e) {
+                    var offset = axis == 'x' ? start - e.clientX : start - e.clientY;
+                    switch(dragDir) {
+                        case 'top':
+                            if(scope.rFlex) { element[0].style.flexBasis = h + (offset * vy) + 'px'; }
+                            else {            element[0].style.height = h + (offset * vy) + 'px'; }
+                            break;
+                        case 'right':
+                            if(scope.rFlex) { element[0].style.flexBasis = w - (offset * vx) + 'px'; }
+                            else {            element[0].style.width = w - (offset * vx) + 'px'; }
+                            break;
+                        case 'bottom':
+                            if(scope.rFlex) { element[0].style.flexBasis = h - (offset * vy) + 'px'; }
+                            else {            element[0].style.height = h - (offset * vy) + 'px'; }
+                            break;
+                        case 'left':
+                            if(scope.rFlex) { element[0].style.flexBasis = w + (offset * vx) + 'px'; }
+                            else {            element[0].style.width = w + (offset * vx) + 'px'; }
+                            break;
+                    }
+                    updateInfo(e);
+                    throttle(function() { scope.$emit("angular-resizable.resizing", info);});
+                };
+                var dragEnd = function(e) {
+                    updateInfo(e);
+                    scope.$emit("angular-resizable.resizeEnd", info);
+                    scope.$apply();
+                    document.removeEventListener('mouseup', dragEnd, false);
+                    document.removeEventListener('mousemove', dragging, false);
+                    element.removeClass('no-transition');
+                };
+                var dragStart = function(e, direction) {
+                    dragDir = direction;
+                    axis = dragDir == 'left' || dragDir == 'right' ? 'x' : 'y';
+                    start = axis == 'x' ? e.clientX : e.clientY;
+                    w = parseInt(style.getPropertyValue("width"));
+                    h = parseInt(style.getPropertyValue("height"));
+
+                    //prevent transition while dragging
+                    element.addClass('no-transition');
+
+                    document.addEventListener('mouseup', dragEnd, false);
+                    document.addEventListener('mousemove', dragging, false);
+
+                    // Disable highlighting while dragging
+                    if(e.stopPropagation) e.stopPropagation();
+                    if(e.preventDefault) e.preventDefault();
+                    e.cancelBubble = true;
+                    e.returnValue = false;
+
+                    updateInfo(e);
+                    scope.$emit("angular-resizable.resizeStart", info);
+                    scope.$apply();
+                };
+
+                for(var i=0;i<dir.length;i++) {
+                    (function () {
+                        var grabber = document.createElement('div'),
+                            direction = dir[i];
+
+                        // add class for styling purposes
+                        grabber.setAttribute('class', 'rg-' + dir[i]);
+                        grabber.innerHTML = inner;
+                        element[0].appendChild(grabber);
+                        grabber.ondragstart = function() { return false }
+                        grabber.addEventListener('mousedown', function(e) {
+                          disabled = (scope.rDisabled == 'true');
+                          if (!disabled && e.which == 1) {
+                            // left mouse click
+                            dragStart(e, direction);
+                          }
+                        }, false);
+                    }())
+                }
+
+            }
+        }
+    });
+// # sourceMappingURL=acquia_lift.debugger.js.map
 //# sourceMappingURL=acquia_lift.debugger.js.map

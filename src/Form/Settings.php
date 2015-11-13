e63862879adc75245e8703ea4ae7f9c3b1725cf4
Yuan Xie <yuan.xie@acquia.com>
2015-11-12 19:33:41 -0500

    LJS-99 Reformat the settings form.

--- src/Form/Settings.php
+++ src/Form/Settings.php
@@ -37,37 +37,44 @@ class Settings extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     $config = $this->config('acquia_lift.settings');
 
-    $form['account_name'] = array(
+    // Credential
+    $form['credential'] = array(
+      '#title' => t('Credential'),
+      '#type' => 'fieldset',
+      '#tree' => TRUE,
+    );
+    $form['credential']['account_name'] = array(
       '#type' => 'textfield',
       '#title' => t('Account Name'),
       '#default_value' => $config->get('account_name'),
       '#required' => TRUE,
     );
-    $form['site_name'] = array(
+    $form['credential']['customer_site'] = array(
       '#type' => 'textfield',
       '#title' => t('Customer Site'),
-      '#default_value' => $config->get('site_name'),
+      '#default_value' => $config->get('customer_site'),
     );
-    $form['api_url'] = array(
+    $form['credential']['api_url'] = array(
       '#type' => 'textfield',
       '#title' => t('API URL'),
       '#field_prefix' => 'http(s)://',
       '#default_value' => $config->get('api_url'),
       '#required' => TRUE,
     );
-    $form['access_key'] = array(
+    $form['credential']['access_key'] = array(
       '#type' => 'textfield',
       '#title' => t('API Access Key'),
       '#default_value' => $config->get('access_key'),
       '#required' => TRUE,
     );
-    $form['secret_key'] = array(
+    $form['credential']['secret_key'] = array(
       '#type' => 'password',
       '#title' => t('API Secret Key'),
       '#default_value' => $config->get('secret_key'),
       '#required' => empty($config->get('secret_key')),
       '#description' => !empty($config->get('secret_key')) ? t('Only necessary if updating') : '',
     );
+
     return parent::buildForm($form, $form_state);
   }
 
@@ -78,15 +85,16 @@ class Settings extends ConfigFormBase {
     $values = $form_state->getValues();
     $config = $this->config('acquia_lift.settings');
 
-    if (!empty($values['secret_key'])) {
-      $config->set('secret_key', $values['secret_key']);
+    // Credential
+    if (!empty($values['credential']['secret_key'])) {
+      $config->set('secret_key', $values['credential']['secret_key']);
     }
+    $config->set('account_name', $values['credential']['account_name'])
+      ->set('customer_site', $values['credential']['customer_site'])
+      ->set('api_url', $values['credential']['api_url'])
+      ->set('access_key', $values['credential']['access_key']);
 
-    $config->set('account_name', $values['account_name'])
-      ->set('site_name', $values['site_name'])
-      ->set('api_url', $values['api_url'])
-      ->set('access_key', $values['access_key'])
-      ->save();
+    $config->save();
 
     parent::submitForm($form, $form_state);
   }

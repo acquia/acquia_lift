e902a8ec759cd2b7ce800fff4d149ef6d60eb0b2
Yuan Xie <yuan.xie@acquia.com>
2015-11-12 21:50:02 -0500

    LJS-99 Implement form fieldset encapsulation.

--- src/Form/Settings.php
+++ src/Form/Settings.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\acquia_lift\Form;
 
+use Drupal\Core\Config\Config;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -36,38 +37,51 @@ class Settings extends ConfigFormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, Request $request = NULL) {
     $config = $this->config('acquia_lift.settings');
+    $form['credential'] = $this->buildCredentialForm($config);
 
-    // Credential
-    $form['credential'] = array(
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * Build credential form.
+   *
+   * @param \Drupal\Core\Config\Config $config
+   *   Acquia Lift Config.
+   *
+   * @return array
+   *   Credential form.
+   */
+  private function buildCredentialForm(Config $config) {
+    $form = array(
       '#title' => t('Credential'),
       '#type' => 'fieldset',
       '#tree' => TRUE,
     );
-    $form['credential']['account_name'] = array(
+    $form['account_name'] = array(
       '#type' => 'textfield',
       '#title' => t('Account Name'),
       '#default_value' => $config->get('account_name'),
       '#required' => TRUE,
     );
-    $form['credential']['customer_site'] = array(
+    $form['customer_site'] = array(
       '#type' => 'textfield',
       '#title' => t('Customer Site'),
       '#default_value' => $config->get('customer_site'),
     );
-    $form['credential']['api_url'] = array(
+    $form['api_url'] = array(
       '#type' => 'textfield',
       '#title' => t('API URL'),
       '#field_prefix' => 'http(s)://',
       '#default_value' => $config->get('api_url'),
       '#required' => TRUE,
     );
-    $form['credential']['access_key'] = array(
+    $form['access_key'] = array(
       '#type' => 'textfield',
       '#title' => t('API Access Key'),
       '#default_value' => $config->get('access_key'),
       '#required' => TRUE,
     );
-    $form['credential']['secret_key'] = array(
+    $form['secret_key'] = array(
       '#type' => 'password',
       '#title' => t('API Secret Key'),
       '#default_value' => $config->get('secret_key'),
@@ -75,27 +89,38 @@ class Settings extends ConfigFormBase {
       '#description' => !empty($config->get('secret_key')) ? t('Only necessary if updating') : '',
     );
 
-    return parent::buildForm($form, $form_state);
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $values = $form_state->getValues();
     $config = $this->config('acquia_lift.settings');
+    $values = $form_state->getValues();
 
-    // Credential
-    if (!empty($values['credential']['secret_key'])) {
-      $config->set('secret_key', $values['credential']['secret_key']);
-    }
-    $config->set('account_name', $values['credential']['account_name'])
-      ->set('customer_site', $values['credential']['customer_site'])
-      ->set('api_url', $values['credential']['api_url'])
-      ->set('access_key', $values['credential']['access_key']);
+    $this->setCredentialValues($config, $values['credential']);
 
     $config->save();
 
     parent::submitForm($form, $form_state);
+  }
+
+  /**
+   * Set credential values.
+   *
+   * @param \Drupal\Core\Config\Config $config
+   *   Acquia Lift Config.
+   * @param array $values
+   *   Credential values.
+   */
+  private function setCredentialValues(Config $config, $values) {
+    if (!empty($values['secret_key'])) {
+      $config->set('secret_key', $values['secret_key']);
+    }
+    $config->set('account_name', $values['account_name'])
+      ->set('customer_site', $values['customer_site'])
+      ->set('api_url', $values['api_url'])
+      ->set('access_key', $values['access_key']);
   }
 }

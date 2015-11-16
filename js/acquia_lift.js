82bfbe0c9d38d900ef5ff2e2f0ffb7e8c72f0066
Yuan Xie <yuan.xie@acquia.com>
2015-11-16 10:40:12 -0500

    LJS-99 Refactoring JavaScript.

--- js/acquia_lift.js
+++ js/acquia_lift.js
@@ -5,18 +5,6 @@ var _tcwq = _tcwq || [];
 
   "use strict";
 
-  function generateTrackingId(){
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g,
-      function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x7|0x8)).toString(16);
-      });
-    return uuid;
-  }
-  settings.pageContext.trackingId = generateTrackingId();
-
   Drupal.behaviors.acquia_lift = {
     attach: function () {
       Drupal.acquia_lift.initialize();
@@ -33,6 +21,7 @@ var _tcwq = _tcwq || [];
     _tcaq.push(['setAccount', settings.credential.account_name, settings.credential.customer_site]);
 
     // Capture view.
+    Drupal.acquia_lift.generateTrackingId();
     _tcaq.push([ 'captureView', 'Content View', settings.pageContext]);
 
     // Capture identity.
@@ -41,22 +30,36 @@ var _tcwq = _tcwq || [];
     }
 
     // Load and run capture scripts.
-    (function() {
-      function async_load() {
-        var script = document.createElement('script'),
-          firstScript = document.getElementsByTagName('script')[0];
-        script.type = 'text/javascript';
-        script.async = true;
-        script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + settings.credential.js_path;
-        firstScript.parentNode.insertBefore(script, firstScript);
-      }
-      if (window.attachEvent) {
-        window.attachEvent('onload', async_load);
-      }
-      else {
-        window.addEventListener('load', async_load, false);
-      }
-    })();
+    if (window.attachEvent) {
+      window.attachEvent('onload', Drupal.acquia_lift.asyncLoad);
+      return;
+    }
+
+    window.addEventListener('load', Drupal.acquia_lift.asyncLoad, false);
+  };
+
+  /**
+   * Generate tracking ID.
+   */
+  Drupal.acquia_lift.generateTrackingId = function () {
+    var d = new Date().getTime();
+    settings.pageContext.trackingId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = (d + Math.random()*16)%16 | 0;
+      d = Math.floor(d/16);
+      return (c=='x' ? r : (r&0x7|0x8)).toString(16);
+    });
+  };
+
+  /**
+   * Load and run capture scripts.
+   */
+  Drupal.acquia_lift.asyncLoad = function () {
+    var script = document.createElement('script'),
+      firstScript = document.getElementsByTagName('script')[0];
+    script.type = 'text/javascript';
+    script.async = true;
+    script.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + settings.credential.js_path;
+    firstScript.parentNode.insertBefore(script, firstScript);
   };
 
 })(jQuery, Drupal, drupalSettings.acquia_lift);

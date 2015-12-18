64520714d51bf9424ca7f1502ccb96cc9d7e5b8d
Yuan Xie <yuan.xie@acquia.com>
2015-12-17 21:39:18 -0500

    LJS-99 Test NodeTypeThumbnailFormHelper class, getForm() function, add comments.

--- tests/src/Unit/Service/Helper/image_style_options.php
+++ tests/src/Unit/Service/Helper/image_style_options.php
@@ -1,10 +1,23 @@
 <?php
+/**
+ * @file
+ * Contains \Drupal\acquia_lift\Service\Helper\ImageStyleOptions.
+ */
 
 namespace Drupal\acquia_lift\Service\Helper {
+  /**
+   * Mock Drupal's image_style_options function.
+   *
+   * @param bool $include_empty
+   * @return array
+   */
   function image_style_options($include_empty = TRUE) {
     return ImageStyleOptions::$returnValue;
   }
 
+  /**
+   * Class ImageStyleOptions to manage image_style_options.
+   */
   class ImageStyleOptions {
     public static $returnValue = [];
   }

288b9fe53a171da2090858252877b5c8e4864eef
Yuan Xie <yuan.xie@acquia.com>
2015-12-30 19:06:04 -0500

    LJS-99 Marking the TestController deprecated. We will remove it when we have automatic credential validation in the Admin Settings form.

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -10,6 +10,14 @@ namespace Drupal\acquia_lift\Controller;
 use Drupal\Core\Controller\ControllerBase;
 
 class TestController extends ControllerBase {
+  /**
+   * Test connection controller.
+   *
+   * @return array
+   *   List of available segments.
+   *
+   * @deprecated This controller is to be removed as soon as we build an automatic connection checker in the admin settings form.
+   */
   public function test() {
     $data_api = \Drupal::service('acquia_lift.service.api.data_api');
     $segments = $data_api->getSegments();

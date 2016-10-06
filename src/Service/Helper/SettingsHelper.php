c723084faaa9c536d02dbd9ae1da8867dea6f780
Yuan Xie <yuan.xie@acquia.com>
2016-10-05 23:15:40 -0400

    LEX-986 Add more validation and error messages.

--- src/Service/Helper/SettingsHelper.php
+++ src/Service/Helper/SettingsHelper.php
@@ -23,19 +23,102 @@ class SettingsHelper {
    *   True if is an invalid credential.
    */
   public static function isInvalidCredential($credential_settings) {
-    // Required credential need to be filled.
-    if (empty($credential_settings['account_id']) ||
-      empty($credential_settings['site_id']) ||
-      empty($credential_settings['assets_url'])
+    if (SELF::isInvalidCredentialAccountId($credential_settings['account_id']) ||
+      SELF::isInvalidCredentialSiteId($credential_settings['site_id']) ||
+      SELF::isInvalidCredentialAssetsUrl($credential_settings['assets_url']) ||
+      SELF::isInvalidCredentialDecisionAPIUrl($credential_settings['decision_api_url']) ||
+      SELF::isInvalidCredentialOauthUrl($credential_settings['oauth_url'])
     ) {
       return TRUE;
     }
 
-    // URLs need to be valid.
-    if (!UrlHelper::isValid($credential_settings['assets_url']) ||
-      !empty($credential_settings['decision_api_url']) && !UrlHelper::isValid($credential_settings['decision_api_url']) ||
-      !empty($credential_settings['oauth_url']) && !UrlHelper::isValid($credential_settings['oauth_url'])
-    ) {
+    return FALSE;
+  }
+
+  /**
+   * Is an invalid credential Account ID. Invalid if:
+   *   1) Missing, or
+   *   2) Not start with a letter and can further only contain alphanumerical characters.
+   *
+   * @param string
+   *   Credential Account ID.
+   * @return boolean
+   *   True if is an invalid credential Account ID.
+   */
+  public static function isInvalidCredentialAccountId($account_id) {
+    if (empty($account_id) || !preg_match('/^[a-zA-Z_][a-zA-Z\\d_]*$/', $account_id)) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Is an invalid credential Site ID. Invalid if:
+   *   1) Missing, or
+   *   2) Not alphanumerical.
+   *
+   * @param string
+   *   Credential Site ID.
+   * @return boolean
+   *   True if is an invalid credential Site ID.
+   */
+  public static function isInvalidCredentialSiteId($site_id) {
+    if (empty($site_id) || !preg_match('/^[a-zA-Z0-9]*$/', $site_id)) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Is an invalid credential Assets URL. Invalid if:
+   *   1) Missing, or
+   *   2) Not a valid URL.
+   *
+   * @param string
+   *   Credential Assets URL.
+   * @return boolean
+   *   True if is an invalid credential Assets URL.
+   */
+  public static function isInvalidCredentialAssetsUrl($assets_url) {
+    if (empty($assets_url) || !UrlHelper::isValid($assets_url)) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Is an invalid credential Decision API URL. Invalid if:
+   *   1) Exist, and
+   *   2) Not a valid URL.
+   *
+   * @param string
+   *   Credential Decision API URL.
+   * @return boolean
+   *   True if is an invalid credential Decision API URL.
+   */
+  public static function isInvalidCredentialDecisionAPIUrl($decision_api_url) {
+    if (!empty($decision_api_url) && !UrlHelper::isValid($decision_api_url)) {
+      return TRUE;
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * Is an invalid credential OAuth URL. Invalid if:
+   *   1) Exist, and
+   *   2) Not a valid URL.
+   *
+   * @param string
+   *   Credential OAuth URL.
+   * @return boolean
+   *   True if is an invalid credential OAuth URL.
+   */
+  public static function isInvalidCredentialOauthUrl($oauth_url) {
+    if (!empty($oauth_url) && !UrlHelper::isValid($oauth_url)) {
       return TRUE;
     }
 

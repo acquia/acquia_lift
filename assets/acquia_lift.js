dd318d594e7055861d47b06f14dbcd1ee3ea7f26
Nick Veenhof <nick.veenhof@gmail.com>
2016-11-24 14:21:00 +0100

    Code formatting

--- assets/acquia_lift.js
+++ assets/acquia_lift.js
@@ -5,30 +5,31 @@
 
 (function ($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    /**
-     * Opens the Acquia Lift Experience Builder, if it is available.
-     */
-    function openExperienceBuilder() {
-        if (window.AcquiaLiftPublicApi) {
-            window.AcquiaLiftPublicApi.activate();
-        }
-        else {
-            alert('Sorry, Acquia Lift could not be found');
-        }
+  /**
+   * Opens the Acquia Lift Experience Builder, if it is available.
+   */
+  function openExperienceBuilder() {
+    if (window.AcquiaLiftPublicApi) {
+      window.AcquiaLiftPublicApi.activate();
     }
+    else {
+      alert('Sorry, Acquia Lift could not be found');
+    }
+    // Do not act on the href link.
+    return false;
+  }
 
-    /**
-     * Attaches the JS test behavior to to weight div.
-     */
-    Drupal.behaviors.acquiaLiftOpenExperienceBuilder = {
-        attach: function (context, settings) {
-            $('#openLiftLink').on('click', function() {
-                openExperienceBuilder();
-                // Do not act on the href link.
-                return false;
-            });
-        }
-    };
+  /**
+   * Attaches the JS test behavior to to weight div.
+   */
+  Drupal.behaviors.acquiaLiftOpenExperienceBuilder = {
+    attach: function (context, settings) {
+      $(context)
+        .find('.toolbar .toolbar-bar #openLiftLink')
+        .once('acquia-lift-open-experience-builder')
+        .on('click', openExperienceBuilder);
+    }
+  };
 })(jQuery, Drupal);

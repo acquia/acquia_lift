4b4c400e51ca04ca9ee4ead5a65164ded41bde79
Yuan Xie <yuan.xie@acquia.com>
2017-08-15 16:00:32 -0400

    LAT-589 | Rename 'feature flags' to 'user permissions'.

--- src/Service/Context/PageContext.php
+++ src/Service/Context/PageContext.php
@@ -95,7 +95,7 @@ class PageContext extends BaseContext {
     'account_id' => 'account_id',
     'site_id' => 'site_id',
     'content_origin' => 'contentOrigin',
-    'feature_flags' => 'featureFlags',
+    'user_permissions' => 'userPermissions',
     'assets_url' => 'liftAssetsURL',
     'decision_api_url' => 'liftDecisionAPIURL',
     'oauth_url' => 'authEndpoint',

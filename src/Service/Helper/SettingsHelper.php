2ad0f76710483d56c320f4680948b42baef439d9
Daniel Kalmar <daniel.kalmar@cheppers.com>
2019-05-07 16:34:03 +0200

    fix syntax error

--- src/Service/Helper/SettingsHelper.php
+++ src/Service/Helper/SettingsHelper.php
@@ -28,7 +28,7 @@ class SettingsHelper {
     if (SELF::isInvalidCredentialAccountId($credential_settings['account_id']) ||
       SELF::isInvalidCredentialSiteId($credential_settings['site_id']) ||
       SELF::isInvalidCredentialAssetsUrl($credential_settings['assets_url']) ||
-      isset($credential_settings['decision_api_url']) && SELF::isInvalidCredentialDecisionAPIUrl($credential_settings['decision_api_url']) ||
+      isset($credential_settings['decision_api_url']) && SELF::isInvalidCredentialDecisionAPIUrl($credential_settings['decision_api_url'])
     ) {
       return TRUE;
     }

0fdd6b7bc8acf1d6e63f63224f8c07baeb6b0ae6
Alex Posidelov <alex.posidelov@gmail.com>
2021-05-06 18:10:33 +0300

    Perz module init; 2 config forms added

---
+++ src/Form/CISSettingsForm.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Drupal\acquia_perz\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Defines the form to configure the Content Index Service connection settings.
+ */
+class CISSettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'acquia_perz_cis_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['acquia_perz.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $settings = $this
+      ->config('acquia_perz.settings');
+    $cis_settings = $settings->get('cis');
+
+    $form['cis'] = [
+      '#type' => 'details',
+      '#tree' => TRUE,
+      '#open' => TRUE,
+    ];
+    $form['cis']['account_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Account ID'),
+      '#default_value' => $cis_settings['account_id'],
+      '#required' => TRUE,
+    ];
+    $form['cis']['environment'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Environment'),
+      '#default_value' => $cis_settings['environment'],
+      '#required' => TRUE,
+    ];
+    $form['cis']['api_key'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#default_value' => $cis_settings['api_key'],
+      '#required' => TRUE,
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $settings = $this->config('acquia_perz.settings');
+    $values = $form_state->getValues()['cis'];
+    $settings->set('cis.account_id', trim($values['account_id']));
+    $settings->set('cis.environment', trim($values['environment']));
+    $settings->set('cis.api_key', trim($values['api_key']));
+    $settings->save();
+    parent::submitForm($form, $form_state);
+  }
+
+}
